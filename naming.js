9d22bd43ab83dc96cb1b223b55f48bc33700293b
iammaliha <malihatasnim26115338@gmail.com>
2026-07-22 20:58:35 +0600

    nameing

--- naming.js
+++ naming.js
@@ -5,3 +5,8 @@ let age = 20;
 //has to be one word.can not contain any space
 
 const her_phone_number = 46222208;//snake case
+const herPhoneNumber = 46222208;//camel case.most use
+const HerPhoneNumber = 46222208;//pascal case
+
+//name cannot start with a number but number could be used anywhere others than the first lette.
+//other than letter name can start with either (_, $)
